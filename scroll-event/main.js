c2229ff04a3b5f0a30f881b213c034c00f0ce568
Nam Nguyen <nguyentnam.061297@gmail.com>
2023-11-25 21:23:18 +0700

    123
    123

--- scroll-event/main.js
+++ scroll-event/main.js
@@ -16,3 +16,26 @@ btn.addEventListener('click', function() {
         behavior: 'smooth'
     });
 });
+
+/////Ẩn / Hiện password
+const btn2 = document.getElementById("btn2");
+const input = document.getElementById("input");
+// btn2.onclick(function () {
+//     if (input.type == "password") {
+//         input.type = "text";
+//         btn2.innerText = "Hide";
+//     } else {
+//         input.type = "password";
+//         btn2.innerText = "Show";
+//     }
+// });
+btn2.addEventListener("click", function () {
+    console.log(input.type);
+    if (input.type == "password") {
+        input.type = "text";
+        btn2.innerText = "Hide";
+    } else {
+        input.type = "password";
+        btn2.innerText = "Show";
+    }
+});
